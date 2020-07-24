1618ee711d78c130f5ca0911628df08a8940cd67
danieltorres31 <danieltorres31@outlook.com>
2020-07-24 18:12:56 -0300

    Extends BaseService on UserService

--- src/main/java/com/inter/api/technicalTest/service/IUserService.java
+++ src/main/java/com/inter/api/technicalTest/service/IUserService.java
@@ -1,6 +1,9 @@
 package com.inter.api.technicalTest.service;
 
-public interface IUserService {
+import com.inter.api.technicalTest.model.User;
+
+public interface IUserService extends IBaseService<User> {
 	
-	public Integer calculatesSingleDigit(String n, Integer k);
+	public Integer calculatesSingleDigit(Long userId, String n, Integer k);
+
 }
--- src/main/java/com/inter/api/technicalTest/service/UserService.java
+++ src/main/java/com/inter/api/technicalTest/service/UserService.java
@@ -1,12 +1,25 @@
 package com.inter.api.technicalTest.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.inter.api.technicalTest.model.User;
+import com.inter.api.technicalTest.repository.ICalculationRepository;
+import com.inter.api.technicalTest.repository.IUserRepository;
+
 @Service
-public class UserService implements IUserService {
+public class UserService extends BaseService<User> implements IUserService {
+	
+	ICalculationRepository calculationRepository;
+	
+	@Autowired
+	public UserService(IUserRepository userRepository, ICalculationRepository calculationRepository) {
+		super(userRepository);
+		this.calculationRepository = calculationRepository;
+	}
 
 	@Override
-	public Integer calculatesSingleDigit(String n, Integer k) {
+	public Integer calculatesSingleDigit(Long userId, String n, Integer k) {
 		
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < k; i++) {
